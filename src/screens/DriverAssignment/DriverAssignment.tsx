077ed0fb4d5858beeed47393120ccccbf578f130
santiago dibene <dibenesantiago@gmail.com>
2023-09-18 15:35:51 -0300

    feat: adding list of avaiable locations and drive locations, with method to assign and remove locations

--- src/screens/DriverAssignment/DriverAssignment.tsx
+++ src/screens/DriverAssignment/DriverAssignment.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { DeliversAndDriversMap } from "./components/DeliversAndDriversMap/DeliversAndDriversMap"
+import { DriverSelector } from "./components/DriverSelector"
 
 // Sample data for delivery locations and drivers
 const deliveryLocations = [
@@ -11,7 +12,7 @@ const deliveryLocations = [
     direction: "la plata calle 51 123456",
   },
   {
-    id: 1,
+    id: 2,
     lat: -34.921318,
     lng: -57.9536,
     assignedTo: null,
@@ -20,41 +21,150 @@ const deliveryLocations = [
   // i will add  more...
 ]
 
-const drivers = [
-  { id: 1, name: "Driver 1" },
-  { id: 2, name: "Driver 2" },
-  { id: 3, name: "Driver 3" },
-  { id: 4, name: "Driver 4" },
-  { id: 5, name: "Driver 5" },
+export const drivers = [
+  { id: 1, name: "Driver 1", color: "pink" },
+  { id: 2, name: "Driver 2", color: "lightblue" },
+  { id: 3, name: "Driver 3", color: "yellow" },
+  { id: 4, name: "Driver 4", color: "lightgreen" },
+  { id: 5, name: "Driver 5", color: "violet" },
 ]
+export interface Driver {
+  id: number
+  name: string
+}
+export interface Location {
+  id: number
+  lat: number
+  lng: number
+  assignedTo: number | null
+  direction: string
+}
 
 export default function DriverAssignment() {
+  const [selectedDriver, setSelectedDriver] = useState<Driver | null>(null)
+  const [locations, setLocations] = useState<Location[]>(
+    () => deliveryLocations
+  )
+
+  const avaiableLocations = locations.filter(
+    location => location.assignedTo == null
+  )
+
+  const currentDriverLocations = locations.filter(
+    location => location.assignedTo === selectedDriver?.id
+  )
+
   return (
     <div>
       <h1>DriverAssigment</h1>
-      <main>
-        <div style={{ display: "grid", gridTemplateColumns: "50% 50%" }}>
+      <main
+        style={{
+          padding: "8px",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "20px",
+          }}
+        >
           <div
             style={{
               display: "grid",
               gridTemplateRows: "min-content auto",
+              gap: "20px",
               height: "80vh",
             }}
           >
-            <div style={{ display: "flex" }}>
-              {drivers.map(driver => (
-                <button>{driver.name}</button>
-              ))}
-            </div>
-            <DeliversAndDriversMap />
+            <DriverSelector
+              selectedDriver={selectedDriver}
+              setSelectedDriver={setSelectedDriver}
+            />
+
+            <DeliversAndDriversMap
+              locations={locations}
+              onClickLocation={assignLocationDriver}
+            />
           </div>
-          <ul>
-            {deliveryLocations.map(location => (
-              <li>{location.direction}</li>
-            ))}
-          </ul>
+
+          <aside>
+            <h2>Avaible locations</h2>
+            <ul>
+              {avaiableLocations.map(location => (
+                <li key={location.id}>
+                  {location.direction}{" "}
+                  {selectedDriver != null && (
+                    <button onClick={() => assignLocationDriver(location.id)}>
+                      assign
+                    </button>
+                  )}
+                </li>
+              ))}
+            </ul>
+
+            {selectedDriver != null && (
+              <>
+                <h2>Driver's locations</h2>
+                {currentDriverLocations.length > 0 ? (
+                  <ul>
+                    {currentDriverLocations.map(location => (
+                      <li key={location.id}>
+                        {location.direction}{" "}
+                        <button
+                          onClick={() => removeLocationDriver(location.id)}
+                        >
+                          remove
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <span style={{ marginLeft: "12px" }}>
+                    Select locations on the map or on the Avaible locations list
+                  </span>
+                )}
+              </>
+            )}
+          </aside>
         </div>
       </main>
     </div>
   )
+
+  function assignLocationDriver(locationId: number) {
+    console.log("locationId", locationId)
+
+    if (selectedDriver != null) {
+      setLocations(locations =>
+        locations.map(locationI => {
+          if (locationI.id === locationId) {
+            return {
+              ...locationI,
+              assignedTo: selectedDriver.id,
+            }
+          } else {
+            return locationI
+          }
+        })
+      )
+    }
+  }
+
+  function removeLocationDriver(locationId: number) {
+    if (selectedDriver != null) {
+      setLocations(locations =>
+        locations.map(locationI => {
+          if (locationI.id === locationId) {
+            return {
+              ...locationI,
+              assignedTo: null,
+            }
+          } else {
+            return locationI
+          }
+        })
+      )
+    }
+  }
 }
--- src/screens/DriverAssignment/components/DeliversAndDriversMap/DeliversAndDriversMap.tsx
+++ src/screens/DriverAssignment/components/DeliversAndDriversMap/DeliversAndDriversMap.tsx
@@ -2,14 +2,20 @@ import React, { ReactElement } from "react"
 import { Status, Wrapper } from "@googlemaps/react-wrapper"
 import { Map } from "./Map"
 import { Marker } from "./Marker"
+import { Location } from "../../DriverAssignment"
 
-export function DeliversAndDriversMap() {
-  // test position la plata
-  const position: google.maps.LatLngLiteral = {
-    lat: -34.921318,
-    lng: -57.953415,
-  }
-  
+interface DeliversAndDriversMapProps {
+  onClickLocation: (location: number) => void
+  locations: Location[]
+}
+
+export function DeliversAndDriversMap({
+  onClickLocation,
+  locations,
+}: DeliversAndDriversMapProps) {
+  // for now i choose one, to improve it, i can calculate a average on all points in lat an lng
+  const mapCenterPosition = { lat: locations[0].lat, lng: locations[0].lng }
+
   return (
     <Wrapper
       apiKey={process.env.REACT_APP_GOOGLE_API_KEY ?? ""}
@@ -17,17 +23,23 @@ export function DeliversAndDriversMap() {
     >
       <Map
         style={{ flexGrow: "1", height: "100%" }}
-        center={position}
+        center={mapCenterPosition}
         zoom={18}
       >
-        <Marker position={position} />
+        {locations.map(location => (
+          <Marker
+            key={location.id}
+            position={{ lat: location.lat, lng: location.lng }}
+            onClick={() => onClickLocation(location.id)}
+          />
+        ))}
       </Map>
     </Wrapper>
   )
 }
 
 const render = (status: Status): ReactElement => {
-  if (status === Status.LOADING) return <h3>{status} ..</h3>
-  if (status === Status.FAILURE) return <h3>{status} ...</h3>
+  if (status === Status.LOADING) return <h3>{status} the map...</h3>
+  if (status === Status.FAILURE) return <h3>{status} to load the map.</h3>
   return <></>
 }
