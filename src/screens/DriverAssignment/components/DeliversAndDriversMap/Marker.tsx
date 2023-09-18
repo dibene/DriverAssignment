3075f57bcb97432af9b8c36f16622c49db729a1f
santiago dibene <dibenesantiago@gmail.com>
2023-09-18 15:30:25 -0300

    feat: add marker onclick option

--- src/screens/DriverAssignment/components/DeliversAndDriversMap/Marker.tsx
+++ src/screens/DriverAssignment/components/DeliversAndDriversMap/Marker.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react"
 
+interface MarkerProps extends google.maps.MarkerOptions {
+  style?: { [key: string]: string }
+  onClick?: (e: google.maps.MapMouseEvent) => void
+}
+
 /** Marker bringin from google maps docs https://developers.google.com/maps/documentation/javascript/react-map  */
-export function Marker(options: google.maps.MarkerOptions) {
+export function Marker({ onClick, style, ...options }: MarkerProps) {
   const [marker, setMarker] = useState<google.maps.Marker>()
 
   useEffect(() => {
@@ -19,9 +24,16 @@ export function Marker(options: google.maps.MarkerOptions) {
 
   useEffect(() => {
     if (marker) {
+      if (onClick) marker.addListener("click", onClick)
       marker.setOptions(options)
     }
-  }, [marker, options])
+
+    return () => {
+      if (marker) {
+        google.maps.event.clearInstanceListeners(marker)
+      }
+    }
+  }, [marker, onClick, options])
 
   return null
 }
